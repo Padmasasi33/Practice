8041b8bee15c5c58ae561558e804c099286de7db
Padma <padmasasi33@gmail.com>
2025-09-08 15:57:44 +0530

    first commit

---
+++ Task1/Base/BrowserSetup.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.Base
+{
+    public class BrowserSetup
+    {
+        protected IWebDriver driver;
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Url = "https://www.flipkart.com/";
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (driver != null)
+            {
+                driver.Dispose();
+            }
+        }
+    }
+}
+
